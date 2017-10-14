e4ad94b7718e95480f070ec45134196c62cbf654
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-10-13 21:19:11 -0300

    Improve gitlab aliases for Go

--- aliases/go.sh
+++ aliases/go.sh
@@ -1,6 +1,33 @@
+fn gitlab_neoway_path(group, project) {
+        return format(
+		"%s/src/gitlab.neoway.com.br/%s/%s",
+		$GOPATH,
+		$group,
+		$project,
+	)
+}
+
+fn changedir(dir) {
+        _, status <= ls $dir >[2] /dev/null
+        if $status == "0" {
+		cd $dir
+		refreshPrompt()
+        }
+	return $status
+}
+
 fn golab(project) {
-        cd $GOPATH + "/src/gitlab.neoway.com.br/datapirates/" + $project
-        refreshPrompt()
+        pirates <= gitlab_neoway_path("datapirates", $project)
+        platform <= gitlab_neoway_path("dataplatform", $project)
+        status <= changedir($pirates)
+        if $status == "0" {
+		return
+        }
+        status <= changedir($platform)
+        if $status == "0" {
+		return
+        }
+        echo "unable to find project: " + $project
 }
 
 fn gohub(project) {
