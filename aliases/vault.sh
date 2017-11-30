d6bf02ca29af46947218257b923b7be543633fc0
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-11-30 13:14:07 -0200

    Remove echo from password

--- aliases/vault.sh
+++ aliases/vault.sh
@@ -4,8 +4,13 @@ fn vault(credname) {
         credential = "/tmp/credential.sh"
 
         rm -f $credential
+
+        stty_orig <= stty -g
+        stty -echo
         print("Enter password: ")
         password <= head -n1 /dev/stdin
+        stty $stty_orig
+
         echo $password | gpg -q --passphrase-fd 0 --batch --output $credential -d $ciphered
         import /tmp/credential
         rm -f $credential
